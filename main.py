95c55223e87bae62b457def51142cbc34079ea84
karolisbagd <karolis.bagd@gmail.com>
2024-02-17 20:44:41 +0000

    Changed function 'deal_card', added new function 'calculate_score'commented old approach

--- main.py
+++ main.py
@@ -1,59 +1,57 @@
 import random
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 def deal_card():
-    return random.choice(cards)
+    """Generates and returns a random card."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
-def game():
-    player_first_draw = random.sample(cards,2)
-    dealer_first_draw = random.sample(cards,2)
-    
-    print(f"Player: {player_first_draw} {sum(player_first_draw)}")
-    print(f"Dealer: {dealer_first_draw} {sum(dealer_first_draw)}")
+players_cards = []
+dealers_cards = []
 
-    player_score = sum(player_first_draw)
-    dealer_score = sum(dealer_first_draw)
-        
-    game_over = False
-    while not game_over:
-        
-        player_score = sum(player_first_draw)
-        dealer_score = sum(dealer_first_draw)
+
+def calculate_score(cards):
+    return sum(cards)
     
-        if player_score == 21 and dealer_score == 21:
-            print("Draw")
-            game_over = True
-        elif player_score == 21:
-            print(f"Player won with {player_score}")
-            game_over = True
-        elif  dealer_score == 21:
-            print(f"You lose, dealer won with {dealer_score}")
-            game_over = True
-        elif player_score > 21:
-            print("You have over 21")
-            game_over = True
-        else: 
-            question = input("Draw another card")
-            if question == "y":
-                player_first_draw.append(deal_card())
-                print(f"Player: {player_first_draw} {sum(player_first_draw)}")
-            elif question == "n":
-                while dealer_score < 17:
-                    dealer_first_draw.append(deal_card())
-                    dealer_score = sum(dealer_first_draw)
-                    print(f"Dealer: {dealer_first_draw} {sum(dealer_first_draw)}")
-                    if dealer_score > 21: 
-                        print(f"Dealer have over 21 with {dealer_score}, Player wins.")
-                        game_over = True
-                    
-            if dealer_score > player_score:
-                 print(f"Dealer wins with: {dealer_first_draw} {sum(dealer_first_draw)}\n while player {player_first_draw} {sum(player_first_draw)}")
-                 game_over = True
-            elif dealer_score == player_score:
-                print("Draw")
-                game_over = True
-            else:
-                 print(f"Player wins with: {player_first_draw} {sum(player_first_draw)}\n while dealer {dealer_first_draw} {sum(dealer_first_draw)}")
-                 game_over = True
-game()
+# player_score = sum(player_first_draw)
+# dealer_score = sum(dealer_first_draw)
+
+# print(f"Player: {player_first_draw} {sum(player_first_draw)}")
+# print(f"Dealer: {dealer_first_draw} {sum(dealer_first_draw)}")
+
+
+
+for _ in range(2):
+    players_cards.append(deal_card)
+    dealers_cards.append(deal_card)
+    
+
+if calculate_score(players_cards) == 21 and calculate_score(dealers_cards) == 21:
+    print("Draw")
+elif calculate_score(players_cards) == 21:
+    print(f"Player won with {calculate_score(players_cards)}")
+elif  dealers_cards == 21:
+    print(f"You lose, dealer won with {calculate_score(dealers_cards)}")
+elif calculate_score(players_cards) > 21:
+    print("You have over 21")
+else: 
+    question = input("Draw another card")
+if question == "y":
+    players_cards.append(deal_card)
+    print(f"Player's hand {calculate_score(players_cards)}")
+elif question == "n":
+    while calculate_score(dealers_cards) < 17:
+        dealers_cards.append(deal_card)
+        print(f"Dealer: {calculate_score(dealers_cards)}")
+        if calculate_score(dealers_cards) > 21: 
+            print(f"Dealer have over 21 with {calculate_score(dealers_cards)}, Player wins.")
+    
+        
+# if dealer_score > player_score:
+#         print(f"Dealer wins with: {dealer_first_draw} {sum(dealer_first_draw)}\n while player {player_first_draw} {sum(player_first_draw)}")
+
+# elif dealer_score == player_score:
+#     print("Draw")
+# else:
+#         print(f"Player wins with: {player_first_draw} {sum(player_first_draw)}\n while dealer {dealer_first_draw} {sum(dealer_first_draw)}")
+
